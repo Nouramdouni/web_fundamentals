3f6c213dbddf5eb34436a9aeb81a3486af31d6dd
unknown <nourhene803@gmail.com>
2024-10-08 18:42:18 +0100

    github

---
+++ Week_2/Day_2/Core/liking likes/par1/liked.js
@@ -0,0 +1,10 @@
+function likes(element){ 
+
+    if (element.innerHTML="likes") 
+    {  
+        i=i+1
+    } 
+    nombre.innerHTML=i
+}
+var nombre=document.querySelector("#likes")
+var i=0
---
+++ Week_2/Day_2/Core/liking likes/par2/network.js
@@ -0,0 +1,21 @@
+function likes(element) {
+   var nombre=document.querySelector("#likes") 
+   var som=Number(nombre.innerHTML)
+      som=som+1
+      nombre.innerHTML=som
+  }
+ 
+
+  function likes2(element) {   
+   var nombre=document.querySelector("#likes2")
+   var som=Number(nombre.innerHTML)
+      som=som+1
+      nombre.innerHTML=som
+  }
+
+function likes3(element) {   
+   var nombre=document.querySelector("#likes3")
+   var som=Number(nombre.innerHTML)
+      som=som+1
+      nombre.innerHTML=som
+}
