37332b4316dc480c663dbecdb5a2a3f0d4f996f5
unknown <nourhene803@gmail.com>
2024-10-09 09:38:49 +0100

    github

--- Week_2/Day_2/Core/liking likes/par2/network.js
+++ Week_2/Day_2/Core/liking likes/par2/network.js
@@ -1,21 +1,7 @@
-function likes(element) {
-   var nombre=document.querySelector("#likes") 
+function likes(x) {
+   var nombre=document.querySelector(x) 
    var som=Number(nombre.innerHTML)
       som=som+1
       nombre.innerHTML=som
   }
  
-
-  function likes2(element) {   
-   var nombre=document.querySelector("#likes2")
-   var som=Number(nombre.innerHTML)
-      som=som+1
-      nombre.innerHTML=som
-  }
-
-function likes3(element) {   
-   var nombre=document.querySelector("#likes3")
-   var som=Number(nombre.innerHTML)
-      som=som+1
-      nombre.innerHTML=som
-}
